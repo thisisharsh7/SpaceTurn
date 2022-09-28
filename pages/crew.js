ab1a447d8ce75b040787680cebd17206bfea2e66
Harsh Kumar <9u.harsh@gmail.com>
2022-09-28 23:52:38 +0530

    project on go

--- pages/crew.js
+++ pages/crew.js
@@ -1,11 +1,32 @@
-
-
-export default function crew(){
-    return(
-        <>
-            <h1>
-                This is crew
-            </h1>
-        </>
-    )
+export default function crew() {
+  return (
+    <section className="py-12 px-32 flex gap-10 ">
+      <h1 className="text-2xl uppercase">02</h1>
+      <div className="flex flex-col gap-20">
+        <h1 className="text-2xl uppercase">Meet your crew</h1>
+        <div className="flex justify-between">
+          <div className="flex flex-col gap-16">
+            <div className="max-w-min flex flex-col gap-3">
+              <h1 className="uppercase text-2xl">Commander</h1>
+              <h1 className="text-5xl uppercase min-w-max pb-12">
+                Douglas Hurley
+              </h1>
+              <p>
+                Douglas Gerald Hurley is an American engineer, former Marine
+                Corps pilot and former NASA astronaut. He launched into space
+                for the third time as commander of Crew Dragon Demo-2.
+              </p>
+            </div>
+          </div>
+        </div>
+        <ul className="flex gap-16 uppercase">
+          <li>Moon</li>
+          <li>Mars</li>
+          <li>Europa</li>
+          <li>Titan</li>
+        </ul>
+      </div>
+      <img src="" alt="" />
+    </section>
+  );
 }
--- pages/technology.js
+++ pages/technology.js
@@ -1,9 +1,32 @@
-
-
-export default function technology(){
-    return(
-        <>
-            <h1>This is technology</h1>
-        </>
-    )
+export default function technology() {
+  return (
+    <section className="py-12 px-32 flex gap-10 ">
+      <h1 className="text-2xl uppercase">03</h1>
+      <div className="flex flex-col gap-20">
+        <h1 className="text-2xl uppercase">Space launch 101</h1>
+        <div className="flex justify-between gap-10">
+        <ul className="flex flex-col gap-8 uppercase text-stone-900">
+          <li className="bg-white px-7 py-5 rounded-full cursor-pointer ">1</li>
+          <li className="bg-white px-7 py-5 rounded-full cursor-pointer">2</li>
+          <li className="bg-white px-7 py-5 rounded-full cursor-pointer">3</li>
+        </ul>
+          <div className="flex flex-col gap-16">
+            <div className="max-w-min flex flex-col gap-1">
+              <h1 className="uppercase text-xl">The terminology...</h1>
+              <h1 className="text-5xl uppercase min-w-max pb-16">
+                Launch vehicle
+              </h1>
+              <p>
+                Douglas Gerald Hurley is an American engineer, former Marine
+                Corps pilot and former NASA astronaut. He launched into space
+                for the third time as commander of Crew Dragon Demo-2.
+              </p>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+      <img src="" alt="" />
+    </section>
+  );
 }
