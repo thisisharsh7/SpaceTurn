4e9bb523409ac9f7d86cd5b77fbb97d724e9601b
Harsh Kumar <9u.harsh@gmail.com>
2022-10-01 16:33:29 +0530

    project on go

--- External/Header.js
+++ External/Header.js
@@ -1,35 +1,72 @@
 import Link from "next/link";
 import Image from "next/image";
-import logo from "../public/assets/shared/logo.svg"
+import logo from "../public/assets/shared/logo.svg";
+import { useRouter } from "next/router";
 
 export default function Header() {
+  const router = useRouter();
   return (
-    <header className="xl:py-2 py-3" >
+    <header className="xl:py-2 py-3">
       <nav className="xl:p-10 p-5 flex items-center justify-between">
-      <div className="flex w-24">
-        <Image src={logo} alt="logo" />
-      </div>
-      <div className=" bg-white w-full h-0.5">
-      </div>
+        <div className="flex w-24">
+          <Image src={logo} alt="logo" />
+        </div>
+        <div className=" bg-white w-full h-0.5"></div>
         <ul className="sm:flex justify-end xl:gap-10 gap-6 pl-5 hidden">
           <li>
             <Link href="/">
-              <a className="flex gap-3 font-normal p-1 xl:text-2xl text-xl"><span>00</span><span>Home</span></a>
+              <a
+                className={
+                  router.pathname == "/"
+                    ? "flex gap-3 font-normal p-1 xl:text-2xl text-xl before:content-[''] before:bg-white before:h-0.5  before:w-full relative before:absolute before:-bottom-1 before:rounded-full"
+                    : "flex gap-3 font-normal p-1 xl:text-2xl text-xl before:content-[''] before:bg-white before:h-0.5 relative before:absolute before:-bottom-1 before:rounded-full "
+                }
+              >
+                <span>00</span>
+                <span>Home</span>
+              </a>
             </Link>
           </li>
           <li>
             <Link href="/destination">
-              <a className="flex gap-3 font-normal p-1 xl:text-2xl text-xl"><span>01</span><span>Destination</span></a>
+              <a
+                className={
+                  router.pathname == "/destination"
+                    ? "flex gap-3 font-normal p-1 xl:text-2xl text-xl before:content-[''] before:bg-white before:h-0.5  before:w-full relative before:absolute before:-bottom-1 before:rounded-full"
+                    : "flex gap-3 font-normal p-1 xl:text-2xl text-xl before:content-[''] before:bg-white before:h-0.5 relative before:absolute before:-bottom-1 before:rounded-full "
+                }
+              >
+                <span>01</span>
+                <span>Destination</span>
+              </a>
             </Link>
           </li>
           <li>
             <Link href="/crew">
-              <a className="flex gap-3 font-normal p-1 xl:text-2xl text-xl"><span>02</span><span>Crew</span></a>
+              <a
+                className={
+                  router.pathname == "/crew"
+                    ? "flex gap-3 font-normal p-1 xl:text-2xl text-xl before:content-[''] before:bg-white before:h-0.5  before:w-full relative before:absolute before:-bottom-1 before:rounded-full"
+                    : "flex gap-3 font-normal p-1 xl:text-2xl text-xl before:content-[''] before:bg-white before:h-0.5 relative before:absolute before:-bottom-1 before:rounded-full "
+                }
+              >
+                <span>02</span>
+                <span>Crew</span>
+              </a>
             </Link>
           </li>
           <li>
             <Link href="/technology">
-              <a className="flex gap-3 font-normal p-1 xl:text-2xl text-xl"><span>03</span><span>Technology</span></a>
+              <a
+                className={
+                  router.pathname == "/technology"
+                    ? "flex gap-3 font-normal p-1 xl:text-2xl text-xl before:content-[''] before:bg-white before:h-0.5  before:w-full relative before:absolute before:-bottom-1 before:rounded-full"
+                    : "flex gap-3 font-normal p-1 xl:text-2xl text-xl before:content-[''] before:bg-white before:h-0.5 relative before:absolute before:-bottom-1 before:rounded-full "
+                }
+              >
+                <span>03</span>
+                <span>Technology</span>
+              </a>
             </Link>
           </li>
         </ul>
--- next.config.js
+++ next.config.js
@@ -5,3 +5,22 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+
+// <!--Chat Bot section-->
+// <div class="Chatobot fixed bottom-5 right-3 bg-transparent  text-white " >
+// <div class="hidden chat-body bg-white">
+// <div class="h-32 w-full">
+//   <div class=" flex gap-20 p-3 bg-blue-700">
+//     <img src="./Assets/Images/logo.jpg" class="rounded-full w-8" alt="logo" >
+//     <p onclick="ToggleChat(this)" class="cursor-pointer">X</p>
+//     </div>
+// </div>
+// </div>
+// <div  class="Bot-body" onclick="ToggleBot(this)">
+// <div class="w-16 text-center cursor-pointer transition ease-in-out delay-100 active:scale-0 duration-200" >
+// <img src="https://freesvg.org/img/1538298822.png"  alt="bot">
+// <p>Ask Me</p>
+// </div>
+// </div>
+// </div>
